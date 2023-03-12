1f30c780448ce1dcc849e8b9ed507ee7467d5555
zynexe <zuhayrraffa@gmail.com>
2023-03-12 17:58:55 +0700

    “menambahkan implementasi dengan state-based  construction”

--- Program.cs
+++ Program.cs
@@ -31,11 +31,89 @@ class KodePos
     }
 }
 
+
+class DoorMachine
+{
+    private State currentState;
+
+    public DoorMachine()
+    {
+        currentState = new LockedState();
+        Console.WriteLine("Pintu terkunci");
+    }
+
+    public void SetState(State state)
+    {
+        currentState = state;
+        currentState.Display();
+    }
+
+    public void OpenDoor()
+    {
+        currentState.OpenDoor(this);
+    }
+
+    public void CloseDoor()
+    {
+        currentState.CloseDoor(this);
+    }
+}
+
+abstract class State
+{
+    public abstract void OpenDoor(DoorMachine doorMachine);
+    public abstract void CloseDoor(DoorMachine doorMachine);
+    public abstract void Display();
+}
+
+class LockedState : State
+{
+    public override void OpenDoor(DoorMachine doorMachine)
+    {
+        Console.WriteLine("Pintu terkunci, tidak bisa dibuka");
+    }
+
+    public override void CloseDoor(DoorMachine doorMachine)
+    {
+        Console.WriteLine("Pintu sudah terkunci");
+    }
+
+    public override void Display()
+    {
+        Console.WriteLine("Pintu terkunci");
+    }
+}
+
+class UnlockedState : State
+{
+    public override void OpenDoor(DoorMachine doorMachine)
+    {
+        Console.WriteLine("Pintu sudah terbuka");
+        doorMachine.SetState(new LockedState());
+    }
+
+    public override void CloseDoor(DoorMachine doorMachine)
+    {
+        Console.WriteLine("Pintu sudah tertutup");
+    }
+
+    public override void Display()
+    {
+        Console.WriteLine("Pintu tidak terkunci");
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
+        //1
         KodePos kodePos = new KodePos();
         Console.WriteLine("Kode Pos Kelurahan Batununggal: " + kodePos.getKodePos("Batununggal"));
+        //2
+        DoorMachine doorMachine = new DoorMachine();
+        doorMachine.OpenDoor(); // mencoba membuka pintu saat terkunci
+        doorMachine.SetState(new UnlockedState()); // membuka pintu
+        doorMachine.CloseDoor(); // menutup pintu
     }
 }
